1432ee6b715221b5035a416bd1797752ddc952bd
KevinOBrien09 <kevinobrien09@hotmail.com>
2023-12-06 00:43:00 +0000

    movebuttonbug

--- Assets/Scripts/MoveButton.cs
+++ Assets/Scripts/MoveButton.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MoveButton:MonoBehaviour                
 {
     public void Move(int dir)
     {
         PlayerManager.inst.movement.StartMove((Dir)dir);
+         EventSystem.current.SetSelectedGameObject(null);
     }
 
     public void Rotate(int rot)
     {
         PlayerManager.inst.movement.rotate.StartRotate((Dir)rot);
+        EventSystem.current.SetSelectedGameObject(null);
     }
 
 }
